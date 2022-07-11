e594c2a8632df71e31a4b7fa400486856a0c62af
Сергей <@DESKTOP-3O9FUQB>
2022-07-11 09:15:34 +0300

    Добавьте файлы проекта.

---
+++ dz_25_06/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class test
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("От: ");
+            int ot = int.Parse(Console.ReadLine());
+            int while1 = ot;
+            Console.Write("До: ");
+            int do1 = int.Parse(Console.ReadLine());
+            Console.Write(while1);
+            while (while1 < do1)
+            {
+                Console.Write(",");
+                while1++;
+                Console.Write(while1);
+            }
+        }
+    }
+}
